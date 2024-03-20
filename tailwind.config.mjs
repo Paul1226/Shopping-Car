cc0583a5a1f3f3ea2b1e3a2d0f459f7128bcb4b3
Paul1226 <paulguachichullca023@gmail.com>
2024-03-20 12:15:43 -0500

    se hizo una nueva lista de colores ordenada

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,32 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				playfair: ['Playfair Display Variable', 'serif'],
-			},
-			colors: {
-				primary500: '#ff6d68',
-				secondary950: '#490806',
-				tertiary200: '#ffc9c750',
-				tertiary200sn: '#ffc9c7',
-				star: '#FFC244',
-				buttonHover600: '#c41711',
-				buttonActive800: '#a11813',
-			},
-			backgroundImage: {
-				'hero':'url("/hero.jpg")',
-				'deg': 'linear-gradient(to right, #741D51 0%, #E44E52 100%)',
-			},
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: ["Playfair Display Variable", "serif"],
+      },
+      colors: {
+        sucentOrange: {
+          '50': "#fff1f1",
+          '100': "#ffe1e0",
+          '200': "#ffc9c7",
+          '300': "#ffa29f",
+          '400': "#ff6d68",
+          '500': "#fb554f",
+          '600': "#e8211a",
+          '700': "#c41711",
+          '800': "#a11813",
+          '900': "#861a16",
+          '950': "#490806",
+        },
+        cardBlur:'#ffffff20',
+        star: '#F5C518',
+      },
+	  
+      backgroundImage: {
+        hero: 'url("/hero.jpg")',
+        deg: "linear-gradient(to right, #741D51 0%, #E44E52 100%)",
+      },
 
-			gridTemplateColumns: {
-				'3p': 'repeat(auto-fill, minmax(300px, 1fr))',
-			},
-			boxShadow: {
-				'shadowCarrito': '0 0 10px 0 #22222250',
-			},
-		},
-	},
-	plugins: [],
-}
+      gridTemplateColumns: {
+        "3p": "repeat(auto-fill, minmax(300px, 1fr))",
+      },
+      boxShadow: {
+        shadowCarrito: "0 0 10px 0 #22222250",
+      },
+    },
+  },
+  plugins: [],
+};
